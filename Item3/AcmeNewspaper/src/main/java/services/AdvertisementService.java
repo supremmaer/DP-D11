0c7b71c501d71833c39ad28867f5b975e8dc903a
ivaperpin <ivanperezpinero96@gmail.com>
2018-04-27 10:59:58 +0200

    Metodo isTaboo para advertisement

--- Item3/AcmeNewspaper/src/main/java/services/AdvertisementService.java
+++ Item3/AcmeNewspaper/src/main/java/services/AdvertisementService.java
@@ -5,25 +5,24 @@ import java.util.Collection;
 
 import javax.transaction.Transactional;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 import repositories.AdvertisementRepository;
-
 import domain.Advertisement;
-
-
 
 @Service
 @Transactional
 public class AdvertisementService {
 
+	@Autowired
+	private ConfigService			configService;
+
 	//Managed Repository ----
 	@Autowired
-	private AdvertisementRepository advertisementRepository;
+	private AdvertisementRepository	advertisementRepository;
+
 
 	//Constructors
 	public AdvertisementService() {
@@ -56,6 +55,9 @@ public class AdvertisementService {
 		Advertisement result;
 
 		result = this.advertisementRepository.save(advertisement);
+		this.isTaboo(result.getTitle(), result);
+		this.isTaboo(result.getBanner(), result);
+		this.isTaboo(result.getTargetPage(), result);
 		return result;
 	}
 
@@ -65,6 +67,23 @@ public class AdvertisementService {
 		result = this.advertisementRepository.findOne(advertisementId);
 		Assert.notNull(result);
 
+		return result;
+	}
+
+	//Other business methods ----------------------------------------------------
+
+	public Boolean isTaboo(final String string, final Advertisement advertisement) {
+		final Boolean result = false;
+		String lower;
+		if (string != null) {
+			lower = string.toLowerCase();
+
+			for (final String s : this.configService.findConfiguration().getTabooWords())
+				if (lower.contains(s)) {
+					advertisement.setTaboo(true);
+					this.advertisementRepository.save(advertisement);
+				}
+		}
 		return result;
 	}
 
